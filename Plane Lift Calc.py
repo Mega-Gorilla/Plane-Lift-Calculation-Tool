1043a1d8fb83b0e23c10c68b086babadda2ef7c6
Mega-Gorilla <hahahadesu@gmail.com>
2020-05-05 16:09:56 +0900

    入力画面追加済み　空気密度自動計算

--- Plane Lift Calc.py
+++ Plane Lift Calc.py
@@ -1,8 +1,22 @@
+
 #defalut value
-Flude_density = 1.2250 #kg/m3
-wing_area = 12 #m2
-Relative_speed = 10 #m/s
+Air_density = 1.2250 #kg/m^3 空気比質量空気密度
+wing_size = 12 #m2 翼面積
+Relative_speed = 10 #m/s 流速
 Angle_of_attack = 2 #迎え角 2は2°下に傾いてる。
+
+print("値を入力してください。/ Please enter a value.")
+print("気圧 日本周辺平均 1013hpa/ Barometric pressure Japan area average 1013hpa" )
+print()
+
+Barometric_pressure = float(input("気圧/Barometric Pressure[hpa]："))
+temp = float(input("気温/Temperature[°C]]:"))
+Air_density = Barometric_pressure/(2.87*(temp+273.15))
+
+#入力データ表示↓
+print("気圧/Barometic Pressure[hpa]",Barometric_pressure)
+print("気温/Temperature[°C]",temp)
+print("空気密度/Air Density[kg/m^3]",Air_density)
 
 #Lift_Coefficient
 CL = 0.7
@@ -14,8 +28,8 @@ CD = 0.02
 #Ｄ＝１４（Ｎ : kg.m/s2）
 
 #calc L
-L = (1/2)*(Flude_density*(Relative_speed**2)*wing_area*CL) #(N)
+L = (1/2)*(Air_density*(Relative_speed**2)*wing_size*CL) #(N)
 #calc D
-D = (1/2)*(Flude_density*(Relative_speed**2)*wing_area*CD) #(N)
+D = (1/2)*(Air_density*(Relative_speed**2)*wing_size*CD) #(N)
 
 print("L=",L,"N  D=",D,"N")
